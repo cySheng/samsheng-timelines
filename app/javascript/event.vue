c049470e501ff2f0fb192de9f9b7fe0e2405d2d8
Samantha Lau <slau@jirnexu.com>
2018-07-16 14:53:37 +0800

    Setup basic vue app for timeline and event component

---
+++ app/javascript/event.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    I'm the event
+    <p>{{ message }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,28 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import Event from '../event.vue'
+import Timeline from '../timeline.vue'
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#app',
+    // data: {
+    //   message: "Can you say hello?"
+    // },
+    components: { Timeline }
+  })
+
+
+})
